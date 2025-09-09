34870195a3623852794595b68a96644a7273a632
Olmosbek510 <olmosbekorazboyev@gmail.com>
2025-09-09 14:16:48 +0500

    unassign users from course

--- src/main/java/com/example/AdminService/service/CourseService.java
+++ src/main/java/com/example/AdminService/service/CourseService.java
@@ -5,7 +5,10 @@ import com.example.AdminService.dto.request.CourseUpdateRequestDTO;
 import com.example.AdminService.dto.response.AssignUsersResponse;
 import com.example.AdminService.dto.response.CourseCreateResponseDTO;
 import com.example.AdminService.dto.response.CourseResponseDTO;
+import com.itechart.profileserviceapi.dto.UserDto;
 import com.itechart.profileserviceapi.dto.UserIdsRequest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 
 import java.util.List;
 import java.util.Map;
@@ -24,4 +27,10 @@ public interface CourseService {
     Map<String, String> deleteById(Long id);
 
     AssignUsersResponse assignUsers(Long courseId, UserIdsRequest request);
+
+    Page<UserDto> findInternsByCourse(Long courseId, PageRequest pageRequest);
+
+    List<UserDto> findMentorsByCourse(Long courseId);
+
+    UserIdsRequest unassignUsers(Long courseId, UserIdsRequest requestBody);
 }
